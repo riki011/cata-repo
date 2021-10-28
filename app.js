b183e6f1c85d60df1d2795f3d2608a8a42ecec6d
Ricardo Genaizir <ricardogenaizir1@gmail.com>
2021-10-27 21:27:22 -0300

    asd

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ let app = express();
 const port = process.env.PORT || 8080
 
 
+app.get('/',(req,resp) => {
+    let inicio = "Proba con la ruta /saludo :)"
+    return resp.json(inicio)
+    });
+    
+
+
 app.get('/saludo',(req,resp) => {
 let saludar = "cata es un drogadicto"
 return resp.json(saludar)
